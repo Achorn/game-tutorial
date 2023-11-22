1323772a9569487a7e3ae89d6b2b31435a23a2ab
achorn <joshuaachorn@gmail.com>
2023-11-22 13:34:31 -0800

    added frames to game

--- Overworld.js
+++ Overworld.js
@@ -3,47 +3,28 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector(".game-canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.map;
+  }
+  startGameLoop() {
+    const step = () => {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.map.drawLowerImage(this.ctx);
+
+      Object.values(this.map.gameObjects).forEach((object) => {
+        object.x += 0.03;
+        object.sprite.draw(this.ctx);
+      });
+
+      this.map.drawUpperImage(this.ctx);
+      requestAnimationFrame(() => {
+        step();
+      });
+    };
+    step();
   }
 
   init() {
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
-    };
-    image.src = "/images/maps/DemoLower.png";
-
-    const shadow = new Image();
-    shadow.onload = () => {
-      this.ctx.drawImage(
-        shadow,
-        0, // left cut
-        0, //top cut
-        32, //width of cut
-        32, //height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    shadow.src = "./images/characters/shadow.png";
-
-    const x = 5;
-    const y = 6;
-    const hero = new Image();
-    hero.onload = () => {
-      this.ctx.drawImage(
-        hero,
-        0, // left cut
-        0, //top cut
-        32, //width of cut
-        32, //height of cut
-        x * 16 - 8,
-        y * 16 - 18,
-        32,
-        32
-      );
-    };
-    hero.src = "./images/characters/people/hero.png";
+    this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+    this.startGameLoop();
   }
 }
