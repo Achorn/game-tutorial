e9478c7db3c52cf8ecd56477d1d5ec5e03150cdc
achorn <joshuaachorn@gmail.com>
2023-11-29 13:45:21 -0800

    added updated fix

--- Overworld.js
+++ Overworld.js
@@ -1,45 +1,52 @@
 class Overworld {
-  constructor(config) {
-    this.element = config.element;
-    this.canvas = this.element.querySelector(".game-canvas");
-    this.ctx = this.canvas.getContext("2d");
-    this.map;
-  }
+ constructor(config) {
+   this.element = config.element;
+   this.canvas = this.element.querySelector(".game-canvas");
+   this.ctx = this.canvas.getContext("2d");
+   this.map = null;
+ }
+
   startGameLoop() {
     const step = () => {
+      //Clear off the canvas
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-      //Establish camera person
+      //Establish the camera person
       const cameraPerson = this.map.gameObjects.hero;
 
-      // Update all objects
-      Object.values(this.map.gameObjects).forEach((object) => {
+      //Update all objects
+      Object.values(this.map.gameObjects).forEach(object => {
         object.update({
           arrow: this.directionInput.direction,
-        });
-      });
-      //Draw lower layer
+          map: this.map,
+        })
+      })
+
+      //Draw Lower layer
       this.map.drawLowerImage(this.ctx, cameraPerson);
 
-      //Draw game Objects
-      Object.values(this.map.gameObjects).forEach((object) => {
+      //Draw Game Objects
+      Object.values(this.map.gameObjects).forEach(object => {
         object.sprite.draw(this.ctx, cameraPerson);
-      });
+      })
 
-      //Draw Upper Layer
+      //Draw Upper layer
       this.map.drawUpperImage(this.ctx, cameraPerson);
+      
       requestAnimationFrame(() => {
-        step();
-      });
-    };
+        step();   
+      })
+    }
     step();
-  }
+ }
 
-  init() {
-    this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
-    this.directionInput = new DirectionInput();
-    this.directionInput.init();
+ init() {
+  this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+  this.map.mountObjects();
 
-    this.startGameLoop();
-  }
+  this.directionInput = new DirectionInput();
+  this.directionInput.init();
+
+  this.startGameLoop();
+ }
 }
--- OverworldMap.js
+++ OverworldMap.js
@@ -1,6 +1,7 @@
 class OverworldMap {
   constructor(config) {
     this.gameObjects = config.gameObjects;
+    this.walls = config.walls || {};
 
     this.lowerImage = new Image();
     this.lowerImage.src = config.lowerSrc;
@@ -8,20 +9,49 @@ class OverworldMap {
     this.upperImage = new Image();
     this.upperImage.src = config.upperSrc;
   }
+
   drawLowerImage(ctx, cameraPerson) {
     ctx.drawImage(
-      this.lowerImage,
-      utils.withGrid(10.5) - cameraPerson.x,
+      this.lowerImage, 
+      utils.withGrid(10.5) - cameraPerson.x, 
       utils.withGrid(6) - cameraPerson.y
-    );
+      )
   }
+
   drawUpperImage(ctx, cameraPerson) {
     ctx.drawImage(
-      this.upperImage,
-      utils.withGrid(10.5) - cameraPerson.x,
+      this.upperImage, 
+      utils.withGrid(10.5) - cameraPerson.x, 
       utils.withGrid(6) - cameraPerson.y
-    );
+    )
+  } 
+
+  isSpaceTaken(currentX, currentY, direction) {
+    const {x,y} = utils.nextPosition(currentX, currentY, direction);
+    return this.walls[`${x},${y}`] || false;
   }
+
+  mountObjects() {
+    Object.values(this.gameObjects).forEach(o => {
+
+      //TODO: determine if this object should actually mount
+      o.mount(this);
+
+    })
+  }
+
+  addWall(x,y) {
+    this.walls[`${x},${y}`] = true;
+  }
+  removeWall(x,y) {
+    delete this.walls[`${x},${y}`]
+  }
+  moveWall(wasX, wasY, direction) {
+    this.removeWall(wasX, wasY);
+    const {x,y} = utils.nextPosition(wasX, wasY, direction);
+    this.addWall(x,y);
+  }
+
 }
 
 window.OverworldMaps = {
@@ -37,28 +67,34 @@ window.OverworldMaps = {
       npc1: new Person({
         x: utils.withGrid(7),
         y: utils.withGrid(9),
-        src: "/images/characters/people/npc1.png",
-      }),
+        src: "/images/characters/people/npc1.png"
+      })
     },
+    walls: {
+      [utils.asGridCoord(7,6)] : true,
+      [utils.asGridCoord(8,6)] : true,
+      [utils.asGridCoord(7,7)] : true,
+      [utils.asGridCoord(8,7)] : true,
+    }
   },
   Kitchen: {
     lowerSrc: "/images/maps/KitchenLower.png",
     upperSrc: "/images/maps/KitchenUpper.png",
     gameObjects: {
       hero: new GameObject({
-        x: utils.withGrid(3),
-        y: utils.withGrid(5),
+        x: 3,
+        y: 5,
       }),
-      npc1: new GameObject({
+      npcA: new GameObject({
         x: 9,
         y: 6,
-        src: "/images/characters/people/npc1.png",
+        src: "/images/characters/people/npc2.png"
       }),
-      npc2: new GameObject({
+      npcB: new GameObject({
         x: 10,
-        y: 4,
-        src: "/images/characters/people/npc2.png",
-      }),
-    },
+        y: 8,
+        src: "/images/characters/people/npc3.png"
+      })
+    }
   },
-};
+}
